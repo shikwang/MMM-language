1b510017097753a2769a096aa0c5efd8dc2367a0
jeremy0jiang <jeremy0jiang@gmail.com>
2018-11-05 17:46:07 -0500

    statement semant edited by shenghao

--- semant.ml
+++ semant.ml
@@ -95,3 +95,42 @@ let check(globals, functions)=
 
     report_duplicate (fun n -> "duplicate local " ^ n ^ " in " ^ func.fname)
       (List.map snd func.locals);
+
+
+
+    
+
+      (* following are edited by Shenghao *)
+      (* Return a semantically-checked statement i.e. containing sexprs *)
+    in
+    
+    let rec check_stmt = function
+        Expr e -> SExpr (expr e)
+      | If(p, b1, b2) -> SIf(check_bool_expr p, check_stmt b1, check_stmt b2)
+      | For(e1, e2, e3, st) -> SFor(expr e1, check_bool_expr e2, expr e3, check_stmt st)
+      | While(p, s) -> SWhile(check_bool_expr p, check_stmt s)
+      | Return e -> let (t, e') = expr e in
+        if t = func.typ then SReturn (t, e') 
+        else raise (Failure ("return gives " ^ string_of_typ t ^ " expected " ^
+                    string_of_typ func.typ ^ " in " ^ string_of_expr e))
+	    
+	    (* A block is correct if each statement is correct and nothing
+	       follows any Return statement.  Nested blocks are flattened. *)
+      | Block sl -> 
+          let rec check_stmt_list = function
+              [Return _ as s] -> [check_stmt s]
+            | Return _ :: _   -> raise (Failure "nothing may follow a return")
+            | Block sl :: ss  -> check_stmt_list (sl @ ss) (* Flatten blocks *)
+            | s :: ss         -> check_stmt s :: check_stmt_list ss
+            | []              -> []
+          in SBlock(check_stmt_list sl)
+
+    in (* body of check_function *)
+    { styp = func.typ;
+      sfname = func.fname;
+      sformals = func.formals;
+      slocals  = func.locals;
+      sbody = match check_stmt (Block func.body) with SBlock(sl) -> sl
+      | _ -> raise (Failure ("internal error: block didn't become a block?"))
+    }
+  in (globals, List.map check_function functions)
